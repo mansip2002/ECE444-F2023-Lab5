94cba16ef4f22a703b88f549c8f8e1b4c1e00b5e
mansip2002 <mansi2156@gmail.com>
2023-10-27 13:08:37 -0400

    Added login required

--- tests/app_test.py
+++ tests/app_test.py
@@ -78,7 +78,11 @@ def test_messages(client):
 
 def test_delete_message(client):
     """Ensure the messages are being deleted"""
-    rv = client.get('/delete/1')
+    rv = client.get("/delete/1")
+    data = json.loads(rv.data)
+    assert data["status"] == 0
+    login(client, app.config["USERNAME"], app.config["PASSWORD"])
+    rv = client.get("/delete/1")
     data = json.loads(rv.data)
     assert data["status"] == 1
 
@@ -87,4 +91,5 @@ def test_search_feature(client):
     """Ensure that the search feature is functional"""
     response = client.get("/search/", content_type="html/text")
     assert response.status_code == 200
+
 
